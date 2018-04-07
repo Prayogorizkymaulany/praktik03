9b22b32c6f9502a8cf8a8f5a273755b432ba15b0
noviaretno <noviaretno>
2018-04-07 11:59:27 +0800

    buat method cetakinfo()

--- src/praktik03/sepatu.java
+++ src/praktik03/sepatu.java
@@ -6,5 +6,10 @@ public class sepatu {
     String ukuran;
     double harga; 
     
-    
+    void cetakinfo(){
+        System.out.println("Merk \t: "+merk+ "\n"+
+                "Warna \t:"+warna+"\n"+
+                "Ukuran \t:"+ukuran+"\n"+
+                "Harga \t:"+harga);
+    }
 }
