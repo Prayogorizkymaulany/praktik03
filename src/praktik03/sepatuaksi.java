41547e71f685f8f34480b845d7b7725639cc95dc
noviaretno <noviaretno>
2018-04-07 13:22:45 +0800

    menggunakan method set

--- src/praktik03/sepatuaksi.java
+++ src/praktik03/sepatuaksi.java
@@ -2,10 +2,13 @@ package praktik03;
 public class sepatuaksi {
     public static void main(String[] args) {
         sepatu s1 = new sepatu();
-        s1.merk="Adidas";
-        s1.warna="Putih";
-        s1.ukuran="22";
-        s1.harga=400000;
+        
+        s1.setMerk("Adidas");
+        s1.setWarna("Putih");
+        s1.setUkuran("22");
+        s1.setHarga(400000);
+        
+    
         
         s1.cetakInfo();
     }
