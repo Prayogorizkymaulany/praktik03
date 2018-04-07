c0ad927754b0c69fde80a1e72dba1705603e221b
noviaretno <noviaretno>
2018-04-07 13:28:16 +0800

    menggunakan method get

--- src/praktik03/sepatuaksi.java
+++ src/praktik03/sepatuaksi.java
@@ -7,10 +7,18 @@ public class sepatuaksi {
         s1.setWarna("Putih");
         s1.setUkuran("22");
         s1.setHarga(400000);
-        
-    
+       
         
         s1.cetakInfo();
+        
+        System.out.println("Merk Sepatu \t:  ");
+        System.out.println(s1.getMerk());
+        System.out.println("Warna Sepatu \t: ");
+        System.out.println(s1.getWarna());
+        System.out.println("Ukuran Sepatu \t: ");
+        System.out.println(s1.getUkuran());
+        System.out.println("Harga Sepatu \t:  ");
+        System.out.println(s1.getHarga());
     }
     
     
