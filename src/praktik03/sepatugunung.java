4b15f7e17810176e5f4c480c7e4e824af1083584
noviaretno <noviaretno>
2018-04-07 15:39:07 +0800

    atribute dan method

--- src/praktik03/sepatugunung.java
+++ src/praktik03/sepatugunung.java
@@ -1,4 +1,12 @@
 package praktik03;
-public class sepatugunung extends sepeda{
+public class sepatugunung extends sepatu{
+    private String jenissepatu;
+    
+    
+    void throttle(){
+        System.out.println("Harganya Larang....");
+        
+    }
+    
     
 }
